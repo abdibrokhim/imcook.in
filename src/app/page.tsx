822ed7f13c12d8082a1b0c3abb36114d936a7904
abdibrokhim <abdibrokhim@gmail.com>
2024-11-22 02:32:10 +0500

    udpate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,31 @@ import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Analytics />
-      <p className="">{`i'm cookin'`}, stay tuned...</p>
-      <p className="">meanwhile, <a href="https://topmate.io/join/abdibrokhim" className="underline text-bold" target="_blank">join topmate.io using this link</a> and get exclusive offers from me!</p>
-      <script
-        src="https://topmate-embed.s3.ap-south-1.amazonaws.com/v1/topmate-embed.js"
-        user-profile="https://topmate.io/embed/profile/abdibrokhim?theme=D5534D"
-        btn-style='{"backgroundColor":"#fff","color":"#000","border":"1px solid #000"}'
-        embed-version="v1"
-        button-text="Book a call"
-        custom-padding="0px"
-        custom-font-size="16px"
-        custom-font-weight="500"
-        custom-width="200px"
-      ></script>
+    <div className=""
+    >
+      <a href="https://theopencommunity.vercel.app/" 
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="grid grid-rows-[20px_1fr_20px] items-center bg-contain bg-no-repeat bg-center relative justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]"
+          style={{ backgroundImage: "url('/intro.png')"}}
+        >
+          <Analytics />
+          <div className="relative z-10 text-center px-4">
+          </div>
+          <script
+            src="https://topmate-embed.s3.ap-south-1.amazonaws.com/v1/topmate-embed.js"
+            user-profile="https://topmate.io/embed/profile/abdibrokhim?theme=D5534D"
+            btn-style='{"backgroundColor":"#fff","color":"#000","border":"1px solid #000"}'
+            embed-version="v1"
+            button-text="Book a call"
+            custom-padding="0px"
+            custom-font-size="16px"
+            custom-font-weight="500"
+            custom-width="200px"
+          ></script>
+        </div>
+      </a>
     </div>
   );
 }
